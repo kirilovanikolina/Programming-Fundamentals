8a9d3ec365721daa7fd4920502a2f55086361795
kirilovanikolina <kirilovanikolina@abv.bg>
2017-12-08 21:35:20 +0200

    Data Types, Variables and Methods - Lab

---
+++ 01. Integer Types.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		byte first = byte.Parse(Console.ReadLine());
+		uint second = uint.Parse(Console.ReadLine());
+		int third = int.Parse(Console.ReadLine());
+		long fourth = long.Parse(Console.ReadLine());
+		
+		Console.WriteLine($"{first} {second} {third} {fourth}");
+	}
+}
