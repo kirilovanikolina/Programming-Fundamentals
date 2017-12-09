9681954f3604e28463c901f7489d21f35faa0546
kirilovanikolina <kirilovanikolina@abv.bg>
2017-12-09 23:26:47 +0200

    Data Types, Variables and Methods - Lab

---
+++ 02. Real Number Types.cs
@@ -0,0 +1,18 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+
+        if (n < 16)
+        {
+            Console.WriteLine(double.Parse(Console.ReadLine()));
+        }
+        else if (n < 29)
+        {
+            Console.WriteLine(decimal.Parse(Console.ReadLine()));
+        }
+	}
+}
