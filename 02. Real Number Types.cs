1af6317811c8cd95fc994a7b332bf832c7ec251b
kirilovanikolina <32369645+kirilovanikolina@users.noreply.github.com>
2017-12-10 10:48:38 +0200

    Data Types, Variables and Methods - Lab

--- 02. Real Number Types.cs
+++ 02. Real Number Types.cs
@@ -4,13 +4,13 @@ public class Program
 	public static void Main()
 	{
 		int n = int.Parse(Console.ReadLine());
-        if (n < 16)
-        {
-            Console.WriteLine(double.Parse(Console.ReadLine()));
-        }
-        else if (n < 29)
-        {
-            Console.WriteLine(decimal.Parse(Console.ReadLine()));
-        }
+        	if (n < 16)
+        	{
+            		Console.WriteLine(double.Parse(Console.ReadLine()));
+        	}
+        	else if (n < 29)
+        	{
+            		Console.WriteLine(decimal.Parse(Console.ReadLine()));
+        	}
 	}
 }
