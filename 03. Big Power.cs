1caaa3b7323906e71e8f1630675f905d283c436d
kirilovanikolina <32369645+kirilovanikolina@users.noreply.github.com>
2017-12-10 10:41:16 +0200

    Data Types, Variables and Methods - Lab

---
+++ 03. Big Power.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Numerics;
+					
+public class Program
+{
+	public static void Main()
+	{
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(BigInteger.Pow(new BigInteger(n), n));
+	}
+}
