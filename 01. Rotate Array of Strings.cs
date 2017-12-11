00e5f2cc78eb711996a605001b61d171cc975d23
kirilovanikolina <32369645+kirilovanikolina@users.noreply.github.com>
2017-12-11 22:08:33 +0200

    Arrays and Lists - Lab

--- 01. Rotate Array of Strings.cs
+++ 01. Rotate Array of Strings.cs
@@ -4,12 +4,12 @@ public class Program
 {
 	public static void Main()
 	{
-		    var array = Console.ReadLine().Split().ToArray();
-        var rotatedArray = new string[array.Length];
-        for (int i = 0; i < array.Length - 1; i++)
-            rotatedArray[i + 1] = array[i];
-        var lastElement = array[rotatedArray.Length - 1];
-        rotatedArray[0] = lastElement;
-        Console.WriteLine(string.Join(" ", rotatedArray));
+		var array = Console.ReadLine().Split().ToArray();
+        	var rotatedArray = new string[array.Length];
+        	for (int i = 0; i < array.Length - 1; i++)
+            		rotatedArray[i + 1] = array[i];
+        	var lastElement = array[rotatedArray.Length - 1];
+        	rotatedArray[0] = lastElement;
+        	Console.WriteLine(string.Join(" ", rotatedArray));
 	}
 }
