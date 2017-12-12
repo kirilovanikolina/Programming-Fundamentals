b8736fefa64a207b0a5021624dc833388e98c22c
kirilovanikolina <32369645+kirilovanikolina@users.noreply.github.com>
2017-12-12 21:49:16 +0200

    Arrays and Lists - Lab

--- 02. Odd Numbers at Odd Positions.cs
+++ 02. Odd Numbers at Odd Positions.cs
@@ -4,14 +4,14 @@ public class Program
 {
 	public static void Main()
 	{
-		    var stringArray = Console.ReadLine().Split().ToArray();
-        var array = new int[stringArray.Length];
-        for (int i = 0; i < array.Length; i++)
-            array[i] = int.Parse(stringArray[i]);
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (i % 2 == 1 && Math.Abs(array[i] % 2) == 1)
-                Console.WriteLine($"Index {i} -> {array[i]}");
-        }
+		var stringArray = Console.ReadLine().Split().ToArray();
+        	var array = new int[stringArray.Length];
+        	for (int i = 0; i < array.Length; i++)
+            		array[i] = int.Parse(stringArray[i]);
+        	for (int i = 0; i < array.Length; i++)
+        	{
+            		if (i % 2 == 1 && Math.Abs(array[i] % 2) == 1)
+                		Console.WriteLine($"Index {i} -> {array[i]}");
+        	}
 	}
 }
