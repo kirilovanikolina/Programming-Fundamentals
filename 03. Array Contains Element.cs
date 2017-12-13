baf86f08441c371809bcc75e044d730ba6d54024
kirilovanikolina <32369645+kirilovanikolina@users.noreply.github.com>
2017-12-13 22:48:34 +0200

    Arrays and Lists - Lab

--- 03. Array Contains Element.cs
+++ 03. Array Contains Element.cs
@@ -4,17 +4,17 @@ public class Program
 {
 	public static void Main()
 	{
-        var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        	var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var element = int.Parse(Console.ReadLine());
-        var containsElement = false;
-        for (int i = 0; i < arr.Length; i++)
-            if (arr[i] == element)
-            {
-                containsElement = true; break;
-            }
-        if (containsElement)
-            Console.WriteLine("yes");
-        else
-            Console.WriteLine("no");
+        	var containsElement = false;
+        	for (int i = 0; i < arr.Length; i++)
+            		if (arr[i] == element)
+            		{
+                		containsElement = true; break;
+            		}
+        	if (containsElement)
+            		Console.WriteLine("yes");
+        	else
+            		Console.WriteLine("no");
 	}
 }
